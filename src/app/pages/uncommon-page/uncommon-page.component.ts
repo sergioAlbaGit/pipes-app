63864eeb46d320925a9c579ff974d8b38cd715b2
sergioAlbaGit <sergioalbatorres@hotmail.com>
2025-04-14 18:50:01 +0200

    Add JsonPipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, SlicePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe } from '@angular/common';
 
 const client1 = {
   name: 'Sergio',
@@ -17,7 +17,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
