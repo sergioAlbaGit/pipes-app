035f10cb407e93cf67cdc42e9f8b3d47a5695df6
sergioAlbaGit <sergioalbatorres@hotmail.com>
2025-04-14 19:16:17 +0200

    Add KeyValue Pipe

--- src/app/pages/uncommon-page/uncommon-page.component.ts
+++ src/app/pages/uncommon-page/uncommon-page.component.ts
@@ -1,6 +1,6 @@
 import { Component, signal } from '@angular/core';
 import { CardComponent } from "../../components/card/card.component";
-import { I18nPluralPipe, I18nSelectPipe, JsonPipe, SlicePipe } from '@angular/common';
+import { I18nPluralPipe, I18nSelectPipe, JsonPipe, KeyValuePipe, SlicePipe, TitleCasePipe } from '@angular/common';
 
 const client1 = {
   name: 'Sergio',
@@ -17,7 +17,7 @@ const client2 = {
 
 @Component({
   selector: 'app-uncommon-page',
-  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe],
+  imports: [CardComponent, I18nSelectPipe, I18nPluralPipe, SlicePipe, JsonPipe, KeyValuePipe, TitleCasePipe],
   templateUrl: './uncommon-page.component.html',
 })
 export default class UncommonPageComponent {
@@ -64,6 +64,13 @@ export default class UncommonPageComponent {
 
   deleteClient(){
     this.clients.update( prev => prev.slice(1))
+  }
+
+  // KeyValue Pipe
+  profile = {
+    name: 'Fernando',
+    age: 36,
+    address: 'Ottawa, Canadá'
   }
 
 }
